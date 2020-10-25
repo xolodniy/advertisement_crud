2881174dfeec73e16c03eb593c2b6401a7d42285
Stepan Morozov <stepan.morozov@atnr.pro>
2020-10-25 20:02:57 +0700

    Add database layer

--- model/model.go
+++ model/model.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"advertisement_crud/etc/common"
+	"errors"
 	"fmt"
 
 	"advertisement_crud/etc/config"
@@ -41,29 +43,164 @@ func New(config config.Database) *Model {
 
 func (m *Model) Migrate() {
 	tx := m.db.Begin()
-	if err := tx.Migrator().CreateTable(&Advertisement{}); err != nil {
-		logrus.WithError(err).Fatal("can't migrate Advertisement table")
+
+	err := tx.Exec(`
+		CREATE TABLE advertisements (
+			id SERIAL PRIMARY KEY,
+			created_at TIMESTAMP NOT NULL DEFAULT now(),
+			updated_at TIMESTAMP NOT NULL DEFAULT now(),
+			deleted_at TIMESTAMP NULL,
+	
+			caption     TEXT NOT NULL,
+			description TEXT NOT NULL,
+			price       INTEGER NOT NULL
+		);
+	`).Error
+	if err != nil {
+		logrus.WithError(err).Fatal("can't migrate advertisements table")
 	}
-	if err := tx.Migrator().CreateTable(&Photo{}); err != nil {
-		logrus.WithError(err).Fatal("can't migrate Photo table")
+
+	err = tx.Exec(`
+		CREATE TABLE photos (
+			id SERIAL PRIMARY KEY,
+			created_at TIMESTAMP NOT NULL DEFAULT now(),
+			updated_at TIMESTAMP NOT NULL DEFAULT now(),
+			deleted_at TIMESTAMP NULL,
+			
+			mime TEXT NOT NULL,
+			body BLOB
+		);
+		`).Error
+	if err != nil {
+		logrus.WithError(err).Fatal("can't migrate photos table")
+	}
+
+	err = tx.Exec(`
+	CREATE TABLE photos_advertisements (
+		photo_id         INTEGER REFERENCES photos,
+		advertisement_id INTEGER REFERENCES advertisements,
+	
+		PRIMARY KEY(photo_id, advertisement_id)
+	);
+	`).Error
+	if err != nil {
+		logrus.WithError(err).Fatal("can't migrate m2m table")
 	}
 	if err := tx.Commit().Error; err != nil {
-		logrus.WithError(err).Fatal("can't commit migrate")
+		logrus.WithError(err).Fatal("can't commit migration")
 	}
+}
+
+func (m *Model) IsMigrated() bool {
+	return m.db.Migrator().HasTable(&Advertisement{})
 }
 
 type Advertisement struct {
 	gorm.Model
 
-	Caption string
-	Price   int
+	Caption     string
+	Description string
+	Price       int
 
-	Photos []Photo
+	Photos []Photo `gorm:"many2many:photos_advertisements;jointable_foreignkey:advertisement_id"`
 }
 
 type Photo struct {
 	gorm.Model
 
-	Body            []byte
-	AdvertisementID int
+	Mime string
+	Body []byte
+}
+
+type PhotosAdvertisements struct {
+	PhotoID         int `gorm:"primary_key"`
+	AdvertisementID int `gorm:"primary_key"`
+}
+
+func (m *Model) GetAdvertisement(id int) (Advertisement, error) {
+	var response Advertisement
+	if err := m.db.Preload("Photos", func(db *gorm.DB) *gorm.DB {
+		return db.Omit("body")
+	}).First(&response, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return Advertisement{}, common.ErrNotFound
+		}
+		logrus.WithError(err).WithField("id", id).Error("can't get advertisement from database")
+		return Advertisement{}, common.ErrInternal
+	}
+	return response, nil
+}
+
+func (m *Model) GetAdvertisements(order string) ([]Advertisement, error) {
+	var response []Advertisement
+	err := m.db.
+		Order(order).
+		Preload("Photos", func(db *gorm.DB) *gorm.DB {
+			return db.Omit("body")
+		}).
+		Find(&response).
+		Error
+	if err != nil {
+		logrus.WithField("order", order).WithError(err).Error("can't get advertisements")
+		return nil, common.ErrInternal
+	}
+	return response, nil
+}
+
+func (m *Model) CreateAdvertisement(advertisement Advertisement, photoIDs []int) (int, error) {
+	tx := m.db.Begin()
+	if err := tx.Create(&advertisement).Error; err != nil {
+		tx.Rollback()
+		logrus.WithError(err).WithField("advertisement", fmt.Sprintf("%+v", advertisement)).
+			Error("can't create advertisement")
+		return 0, common.ErrInternal
+	}
+	for _, id := range photoIDs {
+		err := tx.Create(&PhotosAdvertisements{
+			PhotoID:         id,
+			AdvertisementID: int(advertisement.ID),
+		}).Error
+		if err != nil {
+			tx.Rollback()
+			logrus.WithError(err).WithFields(logrus.Fields{
+				"advertisement": fmt.Sprintf("%+v", advertisement),
+				"photoID":       id,
+			}).Error("can't create link between advertisement and photo")
+			return 0, common.ErrInternal
+		}
+	}
+	if err := tx.Commit().Error; err != nil {
+		logrus.WithError(err).Error("can't commit transaction")
+		return 0, common.ErrInternal
+	}
+	return int(advertisement.ID), nil
+}
+
+func (m *Model) CheckPhotoExist(id int) bool {
+	var i int64
+	if err := m.db.Model(Photo{}).Where("id = ?", id).Count(&i).Error; err != nil {
+		logrus.WithError(err).WithField("id", id).Error("can't count photos in database")
+		return false
+	}
+	return i == 1
+}
+
+func (m *Model) CreatePhoto(photo Photo) (int, error) {
+	if err := m.db.Create(&photo).Error; err != nil {
+		logrus.WithError(err).Error("can't create photo")
+		return 0, common.ErrInternal
+	}
+	return int(photo.ID), nil
+}
+
+func (m *Model) GetPhoto(id int) (Photo, error) {
+	var response Photo
+	if err := m.db.First(&response, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return Photo{}, common.ErrNotFound
+		}
+		logrus.WithError(err).WithField("id", id).Error("can't get photo from database")
+		return Photo{}, common.ErrInternal
+	}
+	return response, nil
 }
